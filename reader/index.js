6af0bb9cd61337dc03dd7d778034ff8968db4c84
Felix Faerber <ffaerber@gmail.com>
2018-11-16 12:37:33 +0100

    remove broker

--- reader/index.js
+++ reader/index.js
@@ -1,12 +1,7 @@
-"use strict";
-const Redis = require('ioredis');
-const MQTT = require('mqtt')
+const axios = require('axios');
 const SerialPort = require("serialport");
 const Readline = require("@serialport/parser-readline");
 const Reading = require("../libs/Reading");
-
-const redis = new Redis(process.env.REDIS_HOST);
-const mqtt = MQTT.connect(process.env.MQTT_BROKER_HOST)
 
 const port = new SerialPort("/dev/ttyUSB0", {
   dataBits: 7
@@ -18,21 +13,20 @@ const parser = port.pipe(
   })
 )
 
-
-mqtt.on('connect', function () {
-  parser.on("data", function (data) {
-    const reading = new Reading(data);
-    if (reading.valid()) {
-      const meterName = `${reading.manufacturerName}_${reading.meterSerialnumber}`
-      const r = {
-        timestamp: Date.now(),
-        energyAMilliwattHour: reading.energyAMilliwattHour,
-        energyBMilliwattHour: reading.energyBMilliwattHour,
-        powerAMilliwatt: reading.powerAMilliwatt,
-        powerBMilliwatt: reading.powerBMilliwatt
-      }
-      redis.sadd("meters", meterName);
-      mqtt.publish(meterName, JSON.stringify(r))
+parser.on("data", function (data) {
+  const reading = new Reading(data);
+  if (reading.valid()) {
+    const meterName = `${reading.manufacturerName}_${reading.meterSerialnumber}`
+    const r = {
+      timestamp: Date.now(),
+      energyAMilliwattHour: reading.energyAMilliwattHour,
+      energyBMilliwattHour: reading.energyBMilliwattHour,
+      powerAMilliwatt: reading.powerAMilliwatt,
+      powerBMilliwatt: reading.powerBMilliwatt
     }
-  })
+
+    axios.post('/meters', JSON.stringify(r))
+      .then(console.log)
+      .catch(console.error)
+  }
 })
